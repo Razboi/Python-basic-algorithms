99461f70bf2edb575666648bc091314f22e3d589
Marc <recatala.marc@gmail.com>
2017-11-13 10:28:24 +0100

    Solved the changeLetters algorithm

---
+++ changeLetters.py
@@ -0,0 +1,23 @@
+"""
+Using the Python language, have the function LetterChanges(str) take the str
+parameter being passed and modify it using the following algorithm. Replace every
+letter in the string with the letter following it in the alphabet.
+Then capitalize every vowel in this new string (a, e, i, o, u) and finally
+return this modified string.
+"""
+
+
+def letter_changes(sen):
+    new_sen = ""
+    for i in sen:
+        if i == "z":
+            new_sen += "A"
+        else:
+            letter = chr(ord(i) + 1)
+            if letter in ["a", "e", "i", "o", "u"]:
+                letter = letter.upper()
+            new_sen += letter
+    return new_sen
+
+
+print(letter_changes("abcdez"))
